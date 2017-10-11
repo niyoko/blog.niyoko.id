d921addba22d47fd8b95acfea747458d0b9ceefc
Niyoko Yuliawan <yoko.kare@gmail.com>
2017-10-11 09:42:51 +0700

    Restore back to old permalink with trailing slash

--- themes/clean-orange/scripts/clean_url_helper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-hexo.extend.helper.register('clean_url', function(path, options){
-  const oldUrl = this.url_for(path, options);
-  if(!oldUrl) return oldUrl;
-  if(!oldUrl.endsWith('.html')) return oldUrl;
-
-  return oldUrl.substring(0, oldUrl.length - 5);
-});
